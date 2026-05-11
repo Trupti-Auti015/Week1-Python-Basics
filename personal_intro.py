98170e38a2dbab11f7dc2bb219fb61dc6daadd4a
Trupti-Auti015 <truptiauti015@gmail.com>
2026-05-11 11:26:35 +0530

    personal_intro uploaded

---
+++ personal_intro.py
@@ -0,0 +1,17 @@
+print("Personal Introduction Program")
+
+name = input("enter your name: ")
+age = input("enter your age: ")
+hobby = input("enter your hobby: ")
+food = input("enter your fav food: ")
+
+print("\nMy Information:")
+print(f"name: {name}")
+print(f"age: {age}")
+print(f"hobby: {hobby}")
+print(f"food: {food}")
+
+print(f"\nHello {name}!")
+print(f"I am {age} years old and live in Pune city.")
+print(f"My hobby is {hobby}.")
+print("Thank you for using this program!")
